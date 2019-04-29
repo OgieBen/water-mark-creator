29253ad46680129653f01924ba52bb3397c1944c
Ogie Ben <bensoft2k5@gmail.com>
2019-04-29 15:27:29 +0100

    added better test with chai-url

--- test/api.test.js
+++ test/api.test.js
@@ -1,14 +1,17 @@
 import request from 'supertest';
 import chai from 'chai';
 import { getWaterMarkImagePath } from '../app/util/imageUtils';
+import chaiUrl from 'chai-url';
 
+chai.use(chaiUrl);
 const assert = chai.assert;
 
 describe('API', () => {
     describe('GET /api/v1/watermark', function() {
         describe('respond with link', function() {
             it("should return link to water marked file", function(done) {
-                this.timeout(5200);
+                // increase test timeout heroku dyno might be asleep
+                this.timeout(6000);
                 request('http://localhost:3000')
                     .post('/api/v1/watermark')
                     .field('name', 'image')
@@ -17,6 +20,7 @@ describe('API', () => {
                     .end(function(err, res) {
                         if (err) return done(err);
                         assert.isString(res.text, 'url');
+                        chai.expect(res.text).to.have.protocol('http');
                         done();
                     });
             });
