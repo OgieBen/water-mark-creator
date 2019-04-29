44555087ede7e9564a891881705256b866830d45
Ogie Ben <bensoft2k5@gmail.com>
2019-04-29 17:58:49 +0100

    added better 404 and 500 handlers

--- app/app.js
+++ app/app.js
@@ -1,12 +1,27 @@
 import express from 'express';
 import rootSchema from './data/schema/rootSchema';
-import  graphqlHttp  from 'express-graphql';
+import graphqlHttp from 'express-graphql';
 import indexRouter from './routes/index'
 
 const app = express();
 
 app.use('/api/v1', indexRouter);
 
-app.use('/graphql', graphqlHttp({schema: rootSchema, graphiql: true}));
+app.use('/graphql', graphqlHttp({
+    schema: rootSchema,
+    graphiql: true
+}));
+
+app.use(function (req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - The page you are looking for does not exist');
+});
+
+app.use(function (err, req, res, next) {
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Internal Server Error');
+});
 
 export default app;
